3121bdcc0e4bd39cf0d1fe98fab60bb80659148f
Dominik <djessen94@gmail.com>
2023-11-10 17:00:04 +0100

    Test GH Action again

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
     <div className="flex flex-col gap-8 items-center justify-center w-4/5 mx-auto py-12">
       <div className="w-full py-8 rounded bg-green-200">Searchbar</div>
       <div className="w-full py-8 rounded bg-pink-200">Current weather section</div>
-      <div className="w-full py-8 rounded bg-amber-200">Forecast section</div>
+      <div className="w-full py-8 rounded bg-amber-200">Forecast next days section</div>
     </div>
   );
 }
