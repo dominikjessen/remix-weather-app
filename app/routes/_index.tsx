44bf705bfcc8f29c4cf8ae6482bc36241c9f9374
Dominik <djessen94@gmail.com>
2023-11-10 16:34:07 +0100

    Set up tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: 'Remix Weather Demo' }, { name: 'description', content: 'Check the weather in...' }];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex flex-col bg-green-200">
+      <h1 className="font-bold text-2xl">A sweet Remix Weather App</h1>
     </div>
   );
 }
