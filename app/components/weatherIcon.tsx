e28233e2c07e30d27e9d48e4264ec47f92fb5639
Dominik <djessen94@gmail.com>
2023-11-16 22:22:20 +0100

    Add fog and haze codes

--- app/components/weatherIcon.tsx
+++ app/components/weatherIcon.tsx
@@ -1,4 +1,4 @@
-import { Cloud, CloudDrizzle, CloudRain, Sun, CloudFog } from 'lucide-react';
+import { Cloud, CloudDrizzle, CloudRain, Sun, CloudFog, Haze } from 'lucide-react';
 
 export interface WeatherIconProps {
   weather_code: number;
@@ -12,8 +12,26 @@ export interface WeatherIconProps {
 // TODO: Map all from 0 - 99
 export default function WeatherIcon({ weather_code, className }: WeatherIconProps) {
   switch (weather_code) {
-    case 0:
+    case 5:
+      return <Haze className={className} />;
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 28:
+    case 40:
+    case 41:
+    case 42:
+    case 43:
+    case 44:
     case 45:
+    case 46:
+    case 47:
+    case 48:
+    case 49:
       return <CloudFog className={className} />;
     case 3:
       return <Cloud className={className} />;
