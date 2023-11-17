82ec5c6736dff355ebde65c9cfac2f2f259bca43
Dominik <djessen94@gmail.com>
2023-11-17 19:33:18 +0100

    Search Combobox

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -1,15 +1,52 @@
 import { useSearchParams } from '@remix-run/react';
 import { SearchIcon } from 'lucide-react';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useDebounce } from '~/hooks/useDebounce';
+import type { SearchResult } from '~/types/weather';
 
 export interface SearchProps {
   onLocationSearched: (newLocation: string) => void;
 }
 
 export default function Search({ onLocationSearched }: SearchProps) {
+  const [searchValue, setSearchValue] = useState('');
+  const debouncedValue = useDebounce(searchValue, 500);
+
+  const [searchOptionsOpen, setSearchOptionsOpen] = useState(false);
+  const [searchResults, setSearchResults] = useState<SearchResult[]>();
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [searchParams, setSearchParams] = useSearchParams();
-  const [searchValue, setSearchValue] = useState('');
+
+  const search = useCallback(async () => {
+    if (!debouncedValue) return;
+
+    const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${debouncedValue}&count=10&language=en&format=json`);
+    const data = await res.json();
+    setSearchResults(data.results as SearchResult[]);
+  }, [debouncedValue]);
+
+  useEffect(() => {
+    if (!debouncedValue) return;
+
+    search();
+    setSearchOptionsOpen(true);
+  }, [debouncedValue, search]);
+
+  function handleOptionSelected(option: SearchResult) {
+    // Set search params
+    const params = new URLSearchParams();
+    params.set('location', `${option.latitude},${option.longitude}`);
+    setSearchParams(params);
+
+    // Notify parent
+    onLocationSearched(`${option.name}, ${option.country_code.toUpperCase()}`);
+
+    // Reset state
+    setSearchValue('');
+    setSearchResults([]);
+    setSearchOptionsOpen(false);
+  }
 
   function geoLocateUser() {
     function success(position: GeolocationPosition) {
@@ -32,15 +69,37 @@ export default function Search({ onLocationSearched }: SearchProps) {
 
   return (
     <div className="w-full flex gap-8 items-center">
-      <div className="flex relative w-full grow">
-        <SearchIcon className="absolute top-[1.3rem] left-[1rem] text-slate-400 h-6 w-6" />
-        <input
-          placeholder="Search for a location"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          className="pl-12 pr-2 py-5 bg-white grow rounded-xl text-lg"
-        />
+      {/* Searchbox with debounced type-ahead search */}
+      <div className="w-full relative grow">
+        <div className="flex relative w-full grow">
+          <SearchIcon className="absolute top-[1.3rem] left-[1rem] text-slate-400 h-6 w-6" />
+          <input
+            placeholder="Search for a location"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="pl-12 pr-2 py-5 bg-white grow rounded-xl text-lg"
+          />
+        </div>
+        {searchResults && searchOptionsOpen && (
+          <ul className="absolute w-full bg-white">
+            {searchResults.map((option) => (
+              <li
+                role="button"
+                key={option.id}
+                onClick={() => handleOptionSelected(option)}
+                className="px-2 py-4 border w-full grid grid-cols-[min-content_1fr] items-center text-slate-700 hover:text-black hover:bg-slate-200 cursor-pointer"
+              >
+                <span className="tracking-wider font-bold text-lg uppercase mr-4 font-mono">{option.country_code}</span>
+                <span className="tracking-wider">
+                  {option.name}, {option.admin1}
+                </span>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
+
+      {/* Get current location */}
       <button className="w-16 h-16 rounded bg-slate-200 hover:bg-slate-300 text-slate-700" onClick={geoLocateUser} title="Use your current location">
         <svg
           xmlns="http://www.w3.org/2000/svg"
--- app/types/weather.d.ts
+++ app/types/weather.d.ts
@@ -57,3 +57,22 @@ export type SingleDayForecast = {
   temperature_2m_max?: number;
   temperature_2m_min?: number;
 };
+
+export type SearchResult = {
+  admin1: string;
+  admin1_id: number;
+  admin2: string;
+  admin2_id: number;
+  country: string;
+  country_code: string;
+  country_id: number;
+  elevation: number;
+  feature_code: string;
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  population: number;
+  postcodes: string[];
+  timezone: string;
+};
